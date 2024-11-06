f940d507e3a305a32075e0fd35aa97da6d6b6531
Jining Liu <jiningdev@gmail.com>
2024-11-06 17:32:16 -0600

    added dashboard image to homeview & modified flexboxes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,13 +23,12 @@ async function logOut() {
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Club Hub</span>
       </a>
       <button data-collapse-toggle="navbar-default" type="button"
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-default" aria-expanded="false">
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-             viewBox="0 0 17 14">
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15" />
+            d="M1 1h15M1 7h15M1 13h15" />
         </svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
@@ -41,7 +40,9 @@ async function logOut() {
           <NavbarLink url="/dashboard/" name="Dashboard" />
           <NavbarLink url="/login/" name="Log In" v-if="!loggedIn" />
           <li v-else>
-            <button class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" type="button" @click="logOut">Log Out</button>
+            <button
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              type="button" @click="logOut">Log Out</button>
           </li>
         </ul>
       </div>
