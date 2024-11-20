d758718dfadb65d3d3f348695467d8dea2729074
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-11-20 16:55:53 -0600

    finish navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,48 +1,82 @@
-<script setup>
+<script setup lang="ts">
 import NavbarLink from "./NavbarLink.vue";
 import { auth } from "../firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { ref } from "vue";
+import { onAuthStateChanged, signOut, type User } from "firebase/auth";
+import { ref, onMounted, onUnmounted } from "vue";
+import { useRoute } from "vue-router";
 
-const loggedIn = ref(false);
+const currentUser = ref<User | null>(null);
+const navbarExpanded = ref(false);
+const route = useRoute();
+const accountMenuExpanded = ref(false);
 
-onAuthStateChanged(auth, currentUser => {
-  loggedIn.value = !!currentUser;
+onAuthStateChanged(auth, user => {
+  currentUser.value = user;
 });
 
 async function logOut() {
   await signOut(auth);
 }
+
+function hideMenus() {
+  accountMenuExpanded.value = false;
+  navbarExpanded.value = false;
+}
+
+onMounted(() => {
+  document.body.addEventListener("click", hideMenus);
+});
+
+onUnmounted(() => {
+  document.body.removeEventListener("click", hideMenus);  
+});
 </script>
 
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-gray-900 sticky top-0">
-    <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-2xl flex flex-wrap items-center mx-auto py-4 relative" @click.stop>
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="/icon.png" class="h-8" alt="Club Hub Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Club Hub</span>
       </a>
       <button data-collapse-toggle="navbar-default" type="button"
-        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-default" aria-expanded="false">
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 mx-auto"
+        aria-controls="navbar-default" aria-expanded="false" @click="navbarExpanded = !navbarExpanded">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M1 1h15M1 7h15M1 13h15" />
         </svg>
       </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <button type="button" class="flex text-sm md:order-2 ms-3 md:ms-6 bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" v-if="!!currentUser" @click="accountMenuExpanded = !accountMenuExpanded">
+        <span class="sr-only">Open user menu</span>
+        <img class="w-8 h-8 rounded-full" :src="currentUser?.photoURL ?? '/icon.png'" alt="user photo">
+      </button>
+      <div :class="{ 'hidden': !navbarExpanded }" class="md:ms-auto me-0 w-full md:block md:w-auto" id="navbar-default">
         <ul
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <NavbarLink url="/" name="Home" active />
-          <NavbarLink url="/explore/" name="Explore Clubs" />
-          <NavbarLink url="/my/" name="My Club" />
-          <NavbarLink url="/dashboard/" name="Dashboard" />
-          <NavbarLink url="/login/" name="Log In" v-if="!loggedIn" />
-          <li v-else>
-            <button
-              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              type="button" @click="logOut">Log Out</button>
+          <NavbarLink url="/" name="Home" :active="route.name === 'home'" />
+          <NavbarLink url="/explore/" name="Explore Clubs" :active="route.name === 'explore'" />
+          <NavbarLink url="/dashboard/" name="Dashboard" :active="route.name === 'dash'" />
+          <NavbarLink url="/login/" name="Log In" v-if="!currentUser" :active="route.name === 'login'" />
+        </ul>
+      </div>
+      <div :class="{ 'hidden': !accountMenuExpanded }" class="z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 absolute right-0 top-12" v-if="currentUser">
+        <div class="py-3 px-4">
+          <span class="block text-sm font-semibold text-gray-900 dark:text-white">{{ currentUser.displayName  }}</span>
+          <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ currentUser.email }}</span>
+        </div>
+        <ul class="py-1 text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
+          <li>
+            <router-link to="/dashboard" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Dashboard</router-link>
+          </li>
+          <li>
+            <router-link to="/account" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Account settings</router-link>
+          </li>
+        </ul>
+        <ul class="py-1 text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
+          <li>
+            <button type="button" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" @click="logOut()">Sign out</button>
           </li>
         </ul>
       </div>
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,32 +1,44 @@
 <script setup lang="ts">
 import { auth } from "../firebase";
-import { onAuthStateChanged, type User } from "firebase/auth";
+import { onAuthStateChanged, type User, deleteUser } from "firebase/auth";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
 
-let user: User | null = null;
+const user = ref<User | null>(null);
 
 onAuthStateChanged(auth, currentUser => {
   if (currentUser) {
-    user = currentUser;
+    user.value = currentUser;
+    console.log(user);
   } else {
     router.push("/login");
   }
 });
+
+async function deleteAccount() {
+  // TODO: Re-authenticate
+  if (user.value)
+    await deleteUser(user.value);
+}
 </script>
 
 <template>
   <section class="bg-gray-50 dark:bg-gray-900 grow px-6 py-8 lg:py-0">
-    <div class="flex items-center gap-3 pb-3 mb-3 border-b border-gray-300 dark:border-gray-700">
-      <template v-if="user?.photoURL">
-        <img :src="user.photoURL" class="align-self-stretch" />
-      </template>
-      <h1 class="text-2xl text-black dark:text-white font-semibold">Welcome, {{ user?.displayName }}</h1>
-    </div>
+    <div class="max-w-screen-2xl mx-auto">
+      <div class="flex items-center gap-3 pb-3 mb-3 border-b border-gray-300 dark:border-gray-700">
+        <template v-if="user?.photoURL">
+          <img :src="user.photoURL" class="align-self-stretch" />
+        </template>
+        <h1 class="text-2xl text-black dark:text-white font-semibold">Welcome, {{ user?.displayName }}</h1>
+      </div>
 
-    <button type="button" class="bg-red-500 text-white p-3 rounded">
-      Delete account
-    </button>
+      <p class="text-black dark:text-white mb-2"><strong class="font-bold">Email: </strong> {{ user?.email }}</p>
+      
+      <p class="text-black dark:text-white mb-3"><strong class="font-bold">Account created: </strong> {{ new Date(Date.parse(user?.metadata.creationTime!)).toLocaleString() }}</p>
+      
+      <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" @click="deleteAccount">Delete account</button>
+    </div>
   </section>
 </template>
