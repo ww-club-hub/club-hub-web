c170d3291ba44448964d487cd7141bf910133b02
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2024-12-27 18:28:39 -0600

    account page updates and onboarding middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import { auth } from "../firebase";
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,19 +15,74 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import("../views/LoginView.vue")
+      component: LoginView,
+      meta: {
+        title: "Login",
+        authProhibited: true
+      }
     },
     {
       path: '/account',
       name: 'account',
-      component: () => import("../views/AccountView.vue")
+      component: () => import("../views/AccountView.vue"),
+      meta: {
+        authRequired: true,
+        title: "My Account"
+      }
     },
     {
       path: '/school/create',
       name: 'create-school',
-      component: () => import("../views/CreateSchoolView.vue")
+      component: () => import("../views/CreateSchoolView.vue"),
+      meta: {
+        authRequired: true,
+        title: "Create School",
+        // this is part of the onboarding flow
+        onboarding: true
+      }
+    },
+    {
+      path: '/account/setup',
+      name: 'onboard',
+      component: () => import("../views/OnboardAccountView.vue"),
+      meta: {
+        authRequired: true,
+        title: "Setup Account",
+        // this is part of the onboarding flow
+        onboarding: true
+      }
     }
   ]
 })
+
+router.beforeEach(async to => {
+  if (to.meta.authRequired && !auth.currentUser) {
+    // redirect to login page
+    return { name: "login", query: { next: to.fullPath } };
+  } else if (to.meta.authProhibited && auth.currentUser) {
+    return { name: "account" };
+  }
+
+  // if they are on an auth-only page (that is not part of the onboarding flow), and they have not onboarded, redirect to onboarding
+  if (to.meta.authRequired && auth.currentUser && !to.meta.onboarding) {
+    return { name: "onboard" };
+  }
+});
+
+onAuthStateChanged(auth, user => {
+  const route = router.currentRoute.value;
+  console.log(route, user);
+  // apply auth guards when the user signs in/out
+  if (route?.meta.authRequired && !user) {
+    router.push({ name: "login", query: { next: route.fullPath } });
+  } else if (route?.meta.authProhibited && user) {
+    // redirect to the next query param or 'account'
+    if (route.query.next) {
+      return router.push(route.query.next as string);
+    } else {
+      return router.push({ name: "account" });
+    }
+  }
+});
 
 export default router
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
 import { auth } from "../firebase";
-import { onAuthStateChanged, type User, deleteUser } from "firebase/auth";
-import { ref } from "vue";
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-
-const user = ref<User | null>(null);
-
-onAuthStateChanged(auth, currentUser => {
-  if (currentUser) {
-    user.value = currentUser;
-  } else {
-    router.push("/login");
-  }
-});
+import { deleteUser } from "firebase/auth";
 
 async function deleteAccount() {
   // TODO: Re-authenticate
-  if (user.value)
-    await deleteUser(user.value);
+  if (auth.currentUser)
+    await deleteUser(auth.currentUser);
 }
 </script>
 
@@ -27,15 +13,15 @@ async function deleteAccount() {
   <section class="bg-gray-50 dark:bg-gray-900 grow">
     <div class="max-w-screen-2xl mx-auto p-4">
       <div class="flex items-center gap-3 pb-3 mb-3 border-b border-gray-300 dark:border-gray-700">
-        <template v-if="user?.photoURL">
-          <img :src="user.photoURL" class="align-self-stretch" />
+        <template v-if="auth.currentUser?.photoURL">
+          <img :src="auth.currentUser.photoURL" class="align-self-stretch" />
         </template>
-        <h1 class="text-2xl text-black dark:text-white font-semibold">Welcome, {{ user?.displayName }}</h1>
+        <h1 class="text-2xl text-black dark:text-white font-semibold">Welcome, {{ auth.currentUser?.displayName }}</h1>
       </div>
 
-      <p class="text-black dark:text-white mb-2"><strong class="font-bold">Email: </strong> {{ user?.email }}</p>
+      <p class="text-black dark:text-white mb-2"><strong class="font-bold">Email: </strong> {{ auth.currentUser?.email }}</p>
       
-      <p class="text-black dark:text-white mb-3"><strong class="font-bold">Account created: </strong> {{ new Date(Date.parse(user?.metadata.creationTime!)).toLocaleString() }}</p>
+      <p class="text-black dark:text-white mb-3"><strong class="font-bold">Account created: </strong> {{ new Date(Date.parse(auth.currentUser?.metadata.creationTime!)).toLocaleString() }}</p>
       
       <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" @click="deleteAccount">Delete account</button>
     </div>
