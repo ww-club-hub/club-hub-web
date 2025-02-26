e5773df6015a7f68ce4cf5f32fe68949d9c21556
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2025-02-26 14:09:16 -0600

    improve rollup chunking

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/@firebase") || id.includes("node_modules/@trpc") || id.includes("utils.ts") || id.includes("schema.ts") || id.includes("firebase.ts") || id.includes("api.ts")) return "common";
+          else if (id.includes("views/club")) return "club";
+          else if (id.includes("views/OnboardAccountView.vue") || id.includes("views/CreateSchoolView.vue")) return "onboard";
+          else if (id.includes("views/Privacy.vue") || id.includes("views/HomeView.vue")) return "static";
+          else if (id.includes("views/AccountView.vue") || id.includes("views/LoginView.vue")) return "auth";
+
+          return null;
+        }
+      }
+    }
   }
 })
