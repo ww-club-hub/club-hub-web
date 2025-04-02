41900d21e9e4dc5b74111dda4779014ca3e60cea
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2025-04-02 17:55:28 -0500

    attendance statistcs

--- src/views/club/AttendanceView.vue
+++ src/views/club/AttendanceView.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import { db } from '@/firebase';
+import type { Club, ClubMeetingAttendance, ClubMeeting, ClubRole } from '@/schema';
+import { typedGetDoc, type DocWithId } from '@/utils';
+import { DocumentReference, doc, setDoc, updateDoc, type DocumentData } from 'firebase/firestore';
+import { onMounted } from 'vue';
+
+const props = defineProps<{
+  role: ClubRole,
+  school: string,
+  club: Club,
+  clubDoc: DocumentReference
+}>();
+
+onMounted(async () => {
+  
+});
+</script>
+
 <template>
-  <h1 class="text-9xl text-gray-700 dark:text-gray-300 text-center pt-30 pb-25">Attendance</h1>
+  <h1 class="text-4xl text-gray-700 dark:text-gray-300 mb-3 font-bold">Attendance Statistics:</h1>
+
 </template>
