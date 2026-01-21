9f5c5d8580177e45ab9ebb56ef09115e67aa23b6
Abhiram Donepudi <abhiram.donepudi@gmail.com>
2026-01-21 23:17:48 +0000

    fixed footer to include current year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="mt-3 py-3 mx-auto text-gray-600 dark:text-gray-300">
-    Copyright &copy; 2025 Club Hub
+    Copyright &copy; {{ currentYear}} Club Hub
     <router-link to="/privacy" class="text-blue-500 hover:underline ms-5">Privacy</router-link>
     <router-link to="/terms" class="text-blue-500 hover:underline ms-5">Terms Of Service</router-link>
   </div>
 </template>
+<script setup lang="ts">
+  const currentYear = new Date().getFullYear();
+</script>
